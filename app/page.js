cec813a3032f9c5783d0e626f466bfe79362ac0e
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2025-01-06 20:29:00 +0200

    Implement basic structure of layout with navigation and Logo

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>The Wild Oasis. Welcome to paradise.</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+
+      <Link href="/cabins">Go to cabins</Link>
+    </div>
+  );
 }
