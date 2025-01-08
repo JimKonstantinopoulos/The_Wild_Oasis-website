c3a6032fec8497b6ae6a6e47cd285f1d6b84a673
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2025-01-08 22:03:03 +0200

    Implement styled Header and lay the basic positioning of children prop

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
-
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefinFont = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: {
@@ -15,13 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefinFont.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
