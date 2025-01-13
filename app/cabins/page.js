393c7a6a61c7035b74f346184fd962f636f38e75
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2025-01-13 22:22:31 +0200

    Implement a communication between server and client by filtering cabins using the capacity

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,15 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
-
-//Revalidate data every one hour
-export const revalidate = 3600;
+import Filter from "../_components/Filter";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,8 +24,13 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      {/* Passing a unique key cause using navigation for the filtering is a React transition is which supsense just waits to load the filtered components and won't display Spinner */}
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
