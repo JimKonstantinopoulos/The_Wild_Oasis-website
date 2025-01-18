f6528107cb6c6c0adcb7faf6d2b976b3838b27de
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2025-01-18 18:24:55 +0200

    Create a custom Login page to call google auth's singIn process

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -1,14 +1,19 @@
+import { signInAction } from "../_lib/actions";
+
 function SignInButton() {
   return (
-    <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
-      <img
-        src='https://authjs.dev/img/providers/google.svg'
-        alt='Google logo'
-        height='24'
-        width='24'
-      />
-      <span>Continue with Google</span>
-    </button>
+    //Authentication needs to happen on the server side so instead of jsut adding onClick here a server action must be executed, that's why the form got created
+    <form action={signInAction}>
+      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -9,15 +9,21 @@ const authConfig = {
     }),
   ],
   callbacks: {
-    //this will check if the user exists to authorize him the access to the pages that have been assigned to middleware
+    //This will check if the user exists to authorize him the access to the pages that have been assigned to middleware
     authorized({ auth, request }) {
-      //convert this value into boolean
+      //Convert this value into boolean
       return !!auth?.user;
     },
+  },
+  pages: {
+    //Assign this page to show instead of the default auth sigin page
+    signIn: "/login",
   },
 };
 
 export const {
   auth,
+  signIn,
+  signOut,
   handlers: { GET, POST },
 } = NextAuth(authConfig);
